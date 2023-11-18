7c7386ba5b0e5f014c7f3f269a9fc3d485f28874
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-11-18 19:48:26 +0200

    Update sort.h

--- sort.h
+++ sort.h
@@ -21,5 +21,6 @@ typedef struct listint_s
 void print_array(const int *, size_t);
 void print_list(const listint_t *);
 void bubble_sort(int *, size_t);
+void selection_sort(int *, size_t);
 
 #endif /* _SORT_H_ */
