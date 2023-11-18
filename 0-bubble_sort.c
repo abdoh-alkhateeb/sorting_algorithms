c54d8a8ded92d7ca3624c35f9cd157b08e474297
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-11-18 19:46:35 +0200

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,28 +9,27 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int temp;
-	size_t i;
-	bool swapped;
+	if (array != NULL && size >= 2)
+	{
+		int temp;
+		size_t i;
+		bool swapped;
 
-	if (array == NULL || size < 2)
-		return;
+		do {
+			swapped = false;
 
-	do {
-		swapped = false;
+			for (i = 0; i < size - 1; i++)
+				if (array[i + 1] < array[i])
+				{
+					temp = array[i + 1];
+					array[i + 1] = array[i];
+					array[i] = temp;
 
-		for (i = 0; i < size - 1; i++)
-		{
-			if (array[i + 1] < array[i])
-			{
-				temp = array[i + 1];
-				array[i + 1] = array[i];
-				array[i] = temp;
+					swapped = true;
 
-				swapped = true;
+					print_array(array, size);
+				}
 
-				print_array(array, size);
-			}
-		}
-	} while (swapped);
+		} while (swapped);
+	}
 }
